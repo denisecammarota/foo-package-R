25ddd1d30e7e661659da92e1021085507cc44937
denisecammarota <denisescammarota@gmail.com>
2022-08-09 12:10:54 -0300

    Initial commit

---
+++ R/my_shannon.R
@@ -0,0 +1,18 @@
+# Shannon diversity function, as provided by the tutorial
+
+#' Shannnon Diversity
+#'
+#' @param x A vector with values of abundance in the community
+#'
+#' @return A numeric vector of length 1 with the value of the Shannon diversity
+#' @export
+#'
+#' @examples
+#' com <- c(1, 2, 3)
+#' my_shannon(com)
+my_shannon <- function(x) {
+  x <- x[x > 0]
+  sum_all <- sum(x)
+  pi <- x/sum_all
+  -sum(pi*log(pi))
+}
